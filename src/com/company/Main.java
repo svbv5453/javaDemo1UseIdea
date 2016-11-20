aa32c922f645eb606fc2cfade94c5bee9656f4ba
SunHeng <svbv5453@163.com>
2016-11-20 12:40:57 +0800

    Initial commit，提交测试

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("测试两下了，太tmd不容易");
+    }
+}
---
+++ src/com/company/demo/Demo1.java
@@ -0,0 +1,12 @@
+package com.company.demo;
+
+/**
+ * Created by Administrator on 2016/11/20.
+ */
+public class Demo1 {
+
+    public void main(String[] args) {
+        System.out.println("测试一下真不容易");
+    }
+
+}
